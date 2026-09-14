0b7d44057e01bc54d6a75e9d1c0ac236ea338424
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-14 18:20:18 -0300

    Criação de excetion personalizadas
    criando o gerenciados de exceções globais e exceções personalizadas.

---
+++ src/main/java/com/vkm_backend/global/exceptions/BusinessException.java
@@ -0,0 +1,20 @@
+package com.vkm_backend.global.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+
+    private int local;
+
+    public BusinessException() {
+        super("BUSINESS_EXCEPTION");
+    }
+
+    public BusinessException(String message, int local) {
+        super(message);
+        this.local = local;
+    }
+
+
+}
