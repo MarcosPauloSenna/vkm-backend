d0073787d21a84b0e2f3d2c2fee593775a6f91b2
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-15 16:43:05 -0300

    feat: ajuste na classe GlobalExceptionHandler. acrescentado a notação  @ExceptionHandler no tratamento das execeções

--- src/main/java/com/vkm_backend/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/vkm_backend/global/handler/GlobalExceptionHandler.java
@@ -5,26 +5,29 @@ import com.vkm_backend.global.exceptions.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 
 @Slf4j(topic = "GLOBAL_EXCEPTION_HANDLER")
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> businessException (BusinessException ex){
         ErrorResponse error  = new ErrorResponse(LocalDateTime.now(),ex.getLocal(), ex.getMessage(), "BUSINESS_EXCEPTION");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 
     }
 
+    @ExceptionHandler(ValidationException.class)
     public ResponseEntity<ErrorResponse> validationException (ValidationException ex){
         ErrorResponse error  = new ErrorResponse(LocalDateTime.now(),201, ex.getMessage(), "VALIDATION_EXCEPTION");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 
     }
-
 
 }
