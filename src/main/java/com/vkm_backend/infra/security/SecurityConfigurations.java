6f612fce4cf8cdf7630f18f3332f4af97a97ee7d
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-15 14:26:00 -0300

    feat: configura autenticacao de usuario

---
+++ src/main/java/com/vkm_backend/infra/security/SecurityConfigurations.java
@@ -0,0 +1,28 @@
+package com.vkm_backend.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,
+                                "/api/user/create").permitAll()
+                        .anyRequest().authenticated())
+                .build();
+
+    }
+}
