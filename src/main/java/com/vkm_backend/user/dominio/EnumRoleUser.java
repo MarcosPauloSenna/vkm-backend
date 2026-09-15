ffb647aff6d2347e6f41330dfc9ce6a915b3443c
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-15 14:23:55 -0300

    chore: configura ambiente docker e postgres

--- src/main/java/com/vkm_backend/user/dominio/EnumRoleUser.java
+++ src/main/java/com/vkm_backend/user/dominio/EnumRoleUser.java
@@ -1,5 +1,14 @@
 package com.vkm_backend.user.dominio;
 
+import lombok.Getter;
+
+@Getter
 public enum EnumRoleUser {
-    ADMIN, USER
+    ADMIN("admin"), USER("user");
+
+    private String role;
+
+    EnumRoleUser(String role) {
+        this.role = role;
+    }
 }
