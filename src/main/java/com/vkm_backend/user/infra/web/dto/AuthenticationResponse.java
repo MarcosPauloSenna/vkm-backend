af5b4cba2de7b8f5c65a321b1a8d8c1983fcaf6a
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-18 15:04:10 -0300

    feat: ajuste na nomenclatura do atributo.

--- src/main/java/com/vkm_backend/user/infra/web/dto/AuthenticationResponse.java
+++ src/main/java/com/vkm_backend/user/infra/web/dto/AuthenticationResponse.java
@@ -1,4 +1,4 @@
 package com.vkm_backend.user.infra.web.dto;
 
-public record AuthenticationResponse(String acessToken, String refreshToken) {
+public record AuthenticationResponse(String accessToken, String refreshToken) {
 }
