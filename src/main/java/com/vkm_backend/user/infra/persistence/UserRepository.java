f090d745c9719ed0912534b31b4f4b12c259c22f
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-18 14:33:26 -0300

    feat: remorção de imports sem uso.

--- src/main/java/com/vkm_backend/user/infra/persistence/UserRepository.java
+++ src/main/java/com/vkm_backend/user/infra/persistence/UserRepository.java
@@ -1,7 +1,7 @@
 package com.vkm_backend.user.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
