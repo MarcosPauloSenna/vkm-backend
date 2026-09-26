8a766e42505f42fac54eb20b09b42ead5f9fa323
Marcos Paulo Santos de Sena <118687390+MarcosPauloSenna@users.noreply.github.com>
2026-09-26 12:19:52 -0300

    feat: alteração de atributos, usar enum GroupActive como tipo do campo active.

---
+++ src/main/java/com/vkm_backend/group/domain/GroupActive.java
@@ -0,0 +1,5 @@
+package com.vkm_backend.group.domain;
+
+public enum GroupActive {
+    INATIVO, ATIVO
+}
--- src/main/java/com/vkm_backend/group/domain/Groups.java
+++ src/main/java/com/vkm_backend/group/domain/Groups.java
@@ -4,6 +4,8 @@ package com.vkm_backend.group.domain;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class Groups {
@@ -13,19 +15,28 @@ public class Groups {
     private String description;
     private String city;
     private String state;
-    private int active;
+    private GroupActive active;
+    private Long createdBy;
+    private LocalDateTime createdAt;
+    private Long updatedBy;
+    private LocalDateTime updatedAt;
 
-    public Groups(Long id, int active, String city, String description, String name, String state) {
-        this.id = id;
+    public Groups(GroupActive active, String city, LocalDateTime createdAt, Long createdBy, String description, Long id, String name, String state, LocalDateTime updatedAt, Long updatedBy) {
         this.active = active;
         this.city = city;
+        this.createdAt = createdAt;
+        this.createdBy = createdBy;
         this.description = description;
+        this.id = id;
         this.name = name;
         this.state = state;
+        this.updatedAt = updatedAt;
+        this.updatedBy = updatedBy;
     }
 
+
     public Groups() {
-        this.active = 1;
+        this.active = GroupActive.ATIVO;
     }
 }
 
